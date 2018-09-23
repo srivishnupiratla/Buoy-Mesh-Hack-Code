7424a0afbd087c051075f2815761a5035a422b5a
Srivishnu Tech <srivishnupiratla@gmail.com>
2018-09-23 14:12:11 -0400

    Update Node-Script.py

--- Node-Script.py
+++ Node-Script.py
@@ -5,35 +5,69 @@ from RFM69registers import *
 import datetime
 import time
 import base64
+import tensorflow as tf
+import sys
+import os
+import shutil
+from os import listdir
+from os import mkdir
+from shutil import copyfile
+from os.path import isfile, join
+import time
+import numpy as np
+import cv2
+import time
+import serial
+import keyboard
+from flask import Flask
+from flask import render_template
+import customfunctions
+
 cap = cv2.VideoCapture(0)
 x=0
 
-def picturetakeandsend:
-        import cv2
-        camera_port = 1
-        ramp_frames = 30
-        camera = cv2.VideoCapture(camera_port)
+def mainpicclass:
+    label_lines = [line.rstrip() for line in tf.gfile.GFile("hotglue/retrainedlabels.txt")]
 
-        def get_image():
+    with tf.gfile.FastGFile("hotglue/retrainedgraph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+    while True:
+        cap = cv2.VideoCapture(1)
+        ret,frame = cap.read()
+        cv2.imwrite('c1.png',frame)
 
-         retval, im = camera.read()
-         return im
+        cap.releaase()
 
-        for i in xrange(ramp_frames):
-         temp = get_image()
-        print("Taking image...")
+        with tf.Session() as sess:
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
-        camera_capture = get_image()
-        file = "/home/Desktop/datapics/"x+1
+        image_data =  tf.gfile.FastGFile('c1.png', 'rb').read()
 
-        cv2.imwrite(file, camera_capture)
+        predictions = sess.run(softmax_tensor, \
+                    {'DecodeJpeg/contents:0': image_data})
 
-        del(camera)
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        firstElt = top_k[0];
 
-        with open("/home/Desktop/datapics"x+1, "rb") as imageFile:
-            base64e = base64.b64encode(imageFile.read())
-            print base64e
-        x+1
+        newFileName = label_lines[firstElt] +"--"+ str(predictions[0][firstElt])[2:7]+".jpg"
+
+        if (label_lines[firstElt] == "cod fish"):
+            print ("cod(S) found, Alert Triggered")
+
+
+
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id].val()
+
+            for valcus in node_id in top_k:
+                return valcus().nums()
+
+
+
+def sendfeed:
 
         rm = RFM69.RFM69(RF69_915MHZ, 1, 1, True)
         print "class initialized"
@@ -46,11 +80,11 @@ def picturetakeandsend:
         print "setting high power"
         rm.setHighPower(True)
         print "Checking temperature"
-        print picture.take
+        print "picture.take"
         print "setting encryption"
         rm.encrypt("1234567891011121")
         print "sending blah to 2"
-        if rm.sendWithRetry(2, ","base64e"," ("".join([chr(letter) for letter in test.DATA]), test.SENDERID, test.RSSI), 3, 20):
+        if rm.sendWithRetry(2, "," (mainpiclass().join(cat /sys/bus/w1/devices/10-000802b4ba0e/w1_slave).join([chr(letter) for letter in test.DATA]), test.SENDERID, test.RSSI), 3, 20):
             print "ack recieved"
         print "reading"
         while True:
@@ -64,6 +98,5 @@ def picturetakeandsend:
 
 
 while true:
-
-    picturetakeandsend
+    sendfeed()
     time.sleep(10000)
